cf58c5715baa5d5f6c0c9fdcf91deaef3c6364c0
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-11-03 23:26:30 -0300

    refactor: (utils.py) Also assert user ids are present

--- src/microservice_social_sweat/services/utils.py
+++ src/microservice_social_sweat/services/utils.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException, Request
 
 
 def verify_user_id_is_the_same_from_jwt(request: Request, user_id: str) -> None:
-    if not user_id == request.state.user_id:
+    if not user_id == request.state.user_id or not request.state.user_id or not user_id:
         raise HTTPException(
             status_code=401,
             detail="Received JWT Token is not from the user id received to perform this action!",
