da2c7dd2ff3a64aafee46ae5ea449e50d2694a58
Miguel Duarte Grieder <migueld.grieder@gmail.com>
2024-11-03 23:25:00 -0300

    fix: (utils.py) Adjust typo

--- src/microservice_social_sweat/services/utils.py
+++ src/microservice_social_sweat/services/utils.py
@@ -5,5 +5,5 @@ def verify_user_id_is_the_same_from_jwt(request: Request, user_id: str) -> None:
     if not user_id == request.state.user_id:
         raise HTTPException(
             status_code=401,
-            detail="Received JWT Token is not from the user id received to peform this action!",
+            detail="Received JWT Token is not from the user id received to perform this action!",
         )
